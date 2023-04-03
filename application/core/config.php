c9fa71b456e697311577af66b292655d32017b2a
Your Name <you@example.com>
2023-04-04 02:07:36 +0500

    add config json

--- application/core/config.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$HOST_DB="localhost";
-$DB_NAME="email";
-$DB_USER="root";
-$DB_PASSWORD="9213";
-?>
--- application/core/model.php
+++ application/core/model.php
@@ -2,15 +2,27 @@
 
 class Model
 {
-
+	protected  $conf;
+	protected  $DBHost;
+	protected  $DBPassword;
+	protected  $DBUser;
+	protected  $DBName;
 
 	function __construct(){
-		
+		$this->conf=json_decode(file_get_contents("application/config/env.json"));
+		$this->DBHost=$this->conf->DBHost;
+		$this->DBPassword=$this->conf->DBPassword;
+		$this->DBUser=$this->conf->DBUser;
+		$this->DBName=$this->conf->DBName;
+
+
 		
 	}
 	
 	public function get_connect_db(){
-		return $conn_db = new PDO("mysql:host=localhost;dbname=email", "root", "9213");
+		return $conn_db = new PDO("mysql:host=".$this->DBHost.";dbname=".$this->DBName, $this->DBUser, $this->DBPassword);
+
+		//return $conn_db = new PDO("mysql:host=localhost;dbname=email", "root", "9213");
 	}
 
 
